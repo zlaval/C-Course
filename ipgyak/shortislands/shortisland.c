0e3f6c50d912a9d32a3979bf1ed97443071ce789
zlaval <zlaval@gmail.com>
2019-09-18 01:29:17 +0200

    fix shortisland

--- ipgyak/shortislands/shortisland.c
+++ ipgyak/shortislands/shortisland.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
@@ -8,53 +7,51 @@ int main()
     scanf("%d", &numberOfMeasures);
 
     int measures[numberOfMeasures];
-    int before = 0;
-    int max = 0;
-    int numberOfLower = 0;
-    int sizeOfLowerArray = 10;
-    int *lowerThenHundred = (int *)calloc(sizeOfLowerArray, sizeof(int));
-    _Bool findFirstZero = 0;
+
     for (int i = 0; i < numberOfMeasures; i++)
     {
-        int actual;
-        scanf("%d", &actual);
-        if(actual==0){
-            findFirstZero=1;
-        }
-        if (findFirstZero==1)
-        {
-            if (before == 0 && actual != 0)
-            {
-                max = actual;
-            }
-            else if (before != 0 && actual != 0 & before < actual)
-            {
-                max = actual;
-            }
-            else if (before != 0 && actual == 0)
-            {
-                if (max < 100)
-                {
-                    if (sizeOfLowerArray == numberOfLower)
-                    {
-                        sizeOfLowerArray += 10;
-                        lowerThenHundred = (int *)realloc(lowerThenHundred, sizeOfLowerArray * sizeof(int));
-                    }
-                    *lowerThenHundred++ = max;
-                    numberOfLower++;
-                }
-            }
-            before = actual;
-        }
+        scanf("%d", &measures[i]);
     }
 
-    lowerThenHundred -= numberOfLower;
+    int numberOfLower = 0;
+    int sizeOfLowerArray = 10;
+    int lowerThenHundred[1000];
+
+    int k = 0;
+    while (k < numberOfMeasures && measures[k] != 0)
+    {
+        k++;
+    }
+
+    int before = 0;
+    int max = 0;
+    for (int i = k; i < numberOfMeasures; i++)
+    {
+        int actual = measures[i];
+        if (before == 0 && actual != 0)
+        {
+            max = actual;
+        }
+        else if (before != 0 && actual != 0 && max < actual)
+        {
+            max = actual;
+        }
+        else if (before != 0 && actual == 0)
+        {
+            if (max < 100)
+            {
+                lowerThenHundred[numberOfLower] = max;
+                numberOfLower++;
+            }
+        }
+        before = actual;
+    }
+
     printf("%d\n", numberOfLower);
     for (int i = 0; i < numberOfLower; i++)
     {
         printf("%d ", lowerThenHundred[i]);
     }
 
-    free(lowerThenHundred);
     return 0;
 }
